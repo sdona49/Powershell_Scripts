de21eb5d240277859bf412d9f48bb076d1aef220
sdona49 <step_hen_123@hotmail.com>
2023-03-16 14:11:21 +1000

    Update install_script.ps1

--- install_script.ps1
+++ install_script.ps1
@@ -1,8 +1,4 @@
-$path = "c:\"
-$log_date = Get-Date -Format "MM-dd-yyyy-HHmmss" #used to timestamp the log file this scrip generates
 $PowershellOutFile = "$env:UserProfile\Downloads\NetExtender\Files\NetExtender-x64-10.2.331.MSI"
 
-
-cmd.exe /c "msiexec /i $PowershellOutFile /norestart /qn netlogon=true /l $path\$log_date-action.log"
 #Run Installation
-#Powershell.exe -ExecutionPolicy Bypass $PowershellOutFile -DeploymentType "Install" -DeployMode "NonInteractive"
+Powershell.exe -ExecutionPolicy Bypass $PowershellOutFile -DeploymentType "Install" -DeployMode "NonInteractive"
