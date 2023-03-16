787e0fcd876d19631964f63d19b615e30dec3a75
sdona49 <step_hen_123@hotmail.com>
2023-03-16 10:38:08 +1000

    Update install_script.ps1

--- install_script.ps1
+++ install_script.ps1
@@ -1,4 +1,4 @@
 $PowershellOutFile = "$env:UserProfile\Downloads\NetExtender\Deploy-NetExtender.ps1"
 
 #Run Installation
-Powershell.exe -ExecutionPolicy Bypass -NoLogo -WindowStyle Hidden $PowershellOutFile -DeploymentType "Install" -DeployMode "NonInteractive"
+Powershell.exe -ExecutionPolicy Bypass $PowershellOutFile -DeploymentType "Install" -DeployMode "NonInteractive"
